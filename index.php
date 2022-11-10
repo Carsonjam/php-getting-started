9c49454e199f59252d040a5f621cc1c841965214
unknown <njuurijaymo@gmail.com>
2022-11-10 13:18:54 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo "Hello World Program";
+
+$myName="James Wachira";
+echo $myName;
+var_dump($myName);
+
+//integers
+$a =30;
+$b ="30";
+$c = true;
+$d = 200.34;
+
+var_dump($a);
+var_dump($c);
+var_dump($c);
+var_dump($d);
+
+echo "My Name is ".$myName. "My mpesa balance is ksh." .$d;
+
+?>
